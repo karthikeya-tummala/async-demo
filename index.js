3b57f8b0391ef69082a84091080b7459e342fc8c
karthikeya-tummala <karthikeyatummala@gmail.com>
2024-11-04 01:37:41 +0530

    Async code to fetch from a database with callback hell

--- index.js
+++ index.js
@@ -1,5 +1,23 @@
 console.log('Before');
-setTimeout(() => {
-    console.log('Reading the database');
-}, 2000);
+fetchUser(1, user => {
+    // console.log('User', user);
+    fetchRepos(user.gitHubUsername, repos => {
+        console.log('Repos', repos);
+    });
+});
 console.log('After');
+
+function fetchUser(id, callback){
+    setTimeout(() => {
+        console.log('Reading user details from database!!!');
+        callback({ id: id, gitHubUsername: 'karthik' });
+    }, 2000);
+}
+
+function fetchRepos(username, callback){
+    setTimeout(() => {
+        console.log(`Fetching repositories under the username ${username}!!!`);
+        callback(['repo 1', 'repo 2', 'repo 3']);
+    }, 2000);
+
+}
