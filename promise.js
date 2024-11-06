7bb7bcac4ab6e8c3085186a50cb339850d50c5fb
karthikeya-tummala <karthikeyatummala@gmail.com>
2024-11-06 22:59:10 +0530

    promise.add vs promise.race

--- promise.js
+++ promise.js
@@ -1,7 +1,7 @@
-const p1 = new Promise((reject) => {
+const p1 = new Promise((resolve) => {
     setTimeout(() => {
         console.log('Async Operation 1');
-        reject(new Error('Failed to run'));
+        resolve(1);
     }, 2000);
 });
 
@@ -13,5 +13,9 @@ const p2 = new Promise((resolve) => {
 });
 
 Promise.all([p1, p2])
+    .then(result => console.log(result))
+    .catch(err => console.log('error', err.message));
+
+Promise.race([p1, p2])
     .then(result => console.log(result))
     .catch(err => console.log('error', err.message));
