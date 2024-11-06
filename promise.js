d291f193d6637463ba7bc4407af0b9ccafc70ea2
karthikeya-tummala <karthikeyatummala@gmail.com>
2024-11-06 22:55:34 +0530

    Promise aggregator method

--- promise.js
+++ promise.js
@@ -1,16 +1,17 @@
-const p1 = new Promise((resolve) => {
+const p1 = new Promise((reject) => {
     setTimeout(() => {
         console.log('Async Operation 1');
-        resolve(1);
+        reject(new Error('Failed to run'));
     }, 2000);
 });
 
 const p2 = new Promise((resolve) => {
-   setTimeout(() => {
-       console.log('Async Operation 2');
-       resolve(2);
-   }, 2000);
+    setTimeout(() => {
+        console.log('Async Operation 2');
+        resolve(2);
+    }, 2000);
 });
 
 Promise.all([p1, p2])
-    .then(result => console.log(result));
+    .then(result => console.log(result))
+    .catch(err => console.log('error', err.message));
